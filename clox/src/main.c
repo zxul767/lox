4351f1830049bf2b03612ce0aa3692d1ae9fbaa4
Willebaldo Gómez <willebaldo.gomez@gmail.com>
2022-10-17 21:54:30 -0500

    Write a proper Makefile
    The major improvement is that we now don't have to manually track
    dependencies for each source file. This is now automatically performed
    as a byproduct of compilation.
    
    The Makefile is now harder to understand, but this is a fine price to
    pay for the benefit of getting reliable builds.
    
    We also changed the layout of the project a bit (see the Makefile for details)

--- clox/src/main.c
+++ clox/src/main.c
@@ -7,48 +7,6 @@
 #include "debug.h"
 #include "vm.h"
 
-void bytecode_sample() {
-  VM vm;
-  vm__init(&vm);
-
-  Bytecode code;
-  bytecode__init(&code);
-
-  int line = 100;
-  // simulate the instructions for:
-  // 100   ((1.2 + (-2.1))
-  // 101    / 2.0)
-  //
-  // load constant: 1.2
-  int index1 = bytecode__store_constant(&code, NUMBER_VAL(1.2));
-  // the OP_CONSTANT instruction takes one argument: the index of the
-  // constant to load (in the code->constants array)
-  bytecode__append(&code, OP_CONSTANT, line);
-  bytecode__append(&code, index1, line);
-  // load constant: 2.1
-  int index2 = bytecode__store_constant(&code, NUMBER_VAL(2.1));
-  bytecode__append(&code, OP_CONSTANT, line);
-  bytecode__append(&code, index2, line);
-  // negation: -2.1
-  bytecode__append(&code, OP_NEGATE, line);
-  // addition: 1.2 + (-2.1)
-  bytecode__append(&code, OP_ADD, line);
-  // load constant: 2.0
-  int index3 = bytecode__store_constant(&code, NUMBER_VAL(2.0));
-  bytecode__append(&code, OP_CONSTANT, line);
-  bytecode__append(&code, index3, line);
-  line++;
-  // division: -0.9 / 2.0
-  bytecode__append(&code, OP_DIVIDE, line);
-  bytecode__append(&code, OP_RETURN, line);
-
-  vm__interpret_bytecode(&code, &vm);
-  /* debug__disassemble(&code, "clox bytecode"); */
-
-  bytecode__dispose(&code);
-  vm__dispose(&vm);
-}
-
 static void repl(VM* vm) {
   char line[1024];
   for (;;) {
