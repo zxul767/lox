6d9c5164c85b8f01bbfdb1303645678d4e7c28d8
Willebaldo Gómez <willebaldo.gomez@gmail.com>
2022-10-21 15:35:05 -0500

    Update RPNPrinter test

--- jlox/app/src/test/java/dev/zxul767/lox/RPNPrinterTest.java
+++ jlox/app/src/test/java/dev/zxul767/lox/RPNPrinterTest.java
@@ -4,21 +4,29 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class RPNPrinterTest {
+  static Expr parse(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+    Parser parser = new Parser(tokens);
+    List<Stmt> statements = parser.parse();
+
+    assert (statements.size() == 1);
+    assert (statements.get(0) instanceof Stmt.Expression);
+
+    Stmt.Expression expr = (Stmt.Expression)statements.get(0);
+    return expr.expression;
+  }
+
   @Test
   void canPrintInRPN() {
-    Expr expression = new Expr.Binary(
-        new Expr.Binary(new Expr.Literal(1), new Token(TokenType.PLUS, "+"),
-                        new Expr.Literal(2)),
-        new Token(TokenType.STAR, "*"),
-        new Expr.Binary(new Expr.Literal(4), new Token(TokenType.MINUS, "-"),
-                        new Expr.Literal(3)));
-
+    Expr expression = parse("(1 + 2) * (4 - 3);");
     RPNPrinter printer = new RPNPrinter();
     String rpn = printer.print(expression);
 
-    assertEquals("1 2 + 4 3 - *", rpn);
+    assertEquals("1.0 2.0 +  4.0 3.0 -  *", rpn);
   }
 }
