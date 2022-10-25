c447f396b7c6e7a8c5bd4f30db6174712f790aab
Willebaldo Gómez <willebaldo.gomez@gmail.com>
2022-10-24 20:47:35 -0500

    Make jlox REPL banner stand out a bit more
    After all, its brother clox REPL is getting pretty sleek thanks to isocline!

--- jlox/app/src/main/java/dev/zxul767/lox/Lox.java
+++ jlox/app/src/main/java/dev/zxul767/lox/Lox.java
@@ -16,10 +16,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import org.jline.builtins.Styles;
 import org.jline.reader.Completer;
-// this is read line support
 import org.jline.reader.EndOfFileException;
 import org.jline.reader.LineReader;
+import org.jline.reader.LineReader.Option;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.reader.UserInterruptException;
 import org.jline.reader.impl.DefaultParser;
@@ -27,6 +28,8 @@ import org.jline.reader.impl.completer.AggregateCompleter;
 import org.jline.reader.impl.completer.StringsCompleter;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import org.jline.utils.AttributedStringBuilder;
+import org.jline.utils.AttributedStyle;
 
 public class Lox {
   private static final Interpreter interpreter = new Interpreter();
@@ -52,20 +55,9 @@ public class Lox {
   }
 
   private static void runPrompt() throws IOException {
-    System.out.println(
-        /* clang-format off */
-          ""
-          + "██╗      ██████╗ ██╗  ██╗    ██████╗ ███████╗██████╗ ██╗" + "\n"
-          + "██║     ██╔═══██╗╚██╗██╔╝    ██╔══██╗██╔════╝██╔══██╗██║" + "\n"
-          + "██║     ██║   ██║ ╚███╔╝     ██████╔╝█████╗  ██████╔╝██║" + "\n"
-          + "██║     ██║   ██║ ██╔██╗     ██╔══██╗██╔══╝  ██╔═══╝ ██║" + "\n"
-          + "███████╗╚██████╔╝██╔╝ ██╗    ██║  ██║███████╗██║     ███████╗" + "\n"
-          + "╚══════╝ ╚═════╝ ╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚═╝     ╚══════╝" + "\n"
-        /* clang-format on */
-    );
-    // source: https://manytools.org/hacker-tools/ascii-banner/
-    System.out.println("Welcome to the Lox REPL. Ready to hack?");
+    Terminal terminal = TerminalBuilder.builder().build();
 
+    showBannerAndHelp(terminal);
     // when running in REPL mode, sometimes there's a race condition
     // between the output and error streams, leading to misleading
     // output that confuses the user as it seems like the input prompt
@@ -88,7 +80,7 @@ public class Lox {
     // same channel:
     System.setErr(System.out);
 
-    LineReader reader = createReplReader();
+    LineReader reader = createReplReader(terminal);
     while (true) {
       try {
         String line = reader.readLine(">>> ").trim();
@@ -141,15 +133,51 @@ public class Lox {
     interpreter.interpret(statements);
   }
 
-  private static LineReader createReplReader() throws IOException {
+  private static void showBannerAndHelp(Terminal terminal) {
+    /* clang-format off */
+    // source: https://manytools.org/hacker-tools/ascii-banner/
+    String banner =
+          ""
+          + "██╗      ██████╗ ██╗  ██╗    ██████╗ ███████╗██████╗ ██╗" + "\n"
+          + "██║     ██╔═══██╗╚██╗██╔╝    ██╔══██╗██╔════╝██╔══██╗██║" + "\n"
+          + "██║     ██║   ██║ ╚███╔╝     ██████╔╝█████╗  ██████╔╝██║" + "\n"
+          + "██║     ██║   ██║ ██╔██╗     ██╔══██╗██╔══╝  ██╔═══╝ ██║" + "\n"
+          + "███████╗╚██████╔╝██╔╝ ██╗    ██║  ██║███████╗██║     ███████╗" + "\n"
+          + "╚══════╝ ╚═════╝ ╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚═╝     ╚══════╝" + "\n";
+    /* clang-format on */
+    String logo =
+        new AttributedStringBuilder()
+            .style(AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW))
+            .style(AttributedStyle.BOLD)
+            .append(banner)
+            .append("Welcome to the Lox REPL. Ready to hack?")
+            .style(AttributedStyle.DEFAULT)
+            .toAnsi();
+    terminal.writer().println(logo);
+
+    terminal.writer().println("- Type \"quit\" to quit. (or use «ctrl-d»)");
+    // TODO: figure out how to do multiline editing (if you copy and paste it
+    // works, but how do we manually type multiple lines?)
+    //
+    // System.out.println(
+    //     "- Use «shift-tab» for multiline input. (or «ctrl-enter», or
+    //     «ctrl-j»)"
+    // );
+    terminal.writer().println("- Use «tab» for word completion");
+    terminal.writer().println("- Use «ctrl-r» to search the history");
+    terminal.writer().println();
+
+    terminal.writer().flush();
+  }
+
+  private static LineReader createReplReader(Terminal terminal)
+      throws IOException {
     // provide completions (triggered via TAB) for all keywords
     Completer completer = new AggregateCompleter(
         new StringsCompleter("quit"),
         new StringsCompleter(StandardLibrary.members.keySet()),
         new StringsCompleter(Scanner.keywords.keySet())
     );
-
-    Terminal terminal = TerminalBuilder.builder().build();
 
     DefaultParser parser = new DefaultParser();
 
